483f50e7fdb8e0d42c545c0e1e3401c05b6d6951
Roger Ellis <ellisr@google.com>
2020-05-13 16:56:10 -0700

    ginkgo: Add exclusion for vendor ELF prebuilts
     - ELF prebuilts were banned in PRODUCT_COPY_FILES starting May 11.
     - Adding BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true to
    BoardConfig.mk temoporarily.
     - See b/156534160 for long-term fix which should override this change.
    
    Test: Build
    Bug: 156534160
    Change-Id: I7d80798d3b2d3818bc835fcede21de2e7ab04735

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,7 +8,9 @@
 BOARD_VENDOR := xiaomi
 DEVICE_PATH := device/xiaomi/ginkgo
 
+# Hacks
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Architecture
 TARGET_ARCH := arm64
