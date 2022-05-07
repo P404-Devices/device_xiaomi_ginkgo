325bbf51c2cc440a843160a472c9eece5e54cbc7
dev-harsh1998 <god@hyper-labs.tech>
2022-05-07 14:27:32 +0800

    ginkgo: Compile HWUI for performance
    * Activates few optimizing compiler flags for HWUI.
    * -fno-omit-frame-pointer, -marm, -mapcs
    
    Change-Id: Ia1391b1a6cbfd876459042f88e58f2eafa17d9d6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,6 +66,9 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 ODM_MANIFEST_WILLOW_FILES := $(DEVICE_PATH)/configs/hidl/manifest_willow.xml
 ODM_MANIFEST_SKUS += willow
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_ginkgo
 TARGET_RECOVERY_DEVICE_MODULES := libinit_ginkgo
