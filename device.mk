f9c059449609c4a47787361b48f6344bfc7e4f5b
hpnightowl <iamhp2k@gmail.com>
2023-02-16 22:54:37 +0530

    ginkgo: switch to vibration qcom component

--- device.mk
+++ device.mk
@@ -192,6 +192,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     init \
     media \
     telephony \
+    vibrator \
     wfd \
     wlan
 
@@ -242,13 +243,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
-
-# Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/input/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Wifi
 PRODUCT_COPY_FILES += \
