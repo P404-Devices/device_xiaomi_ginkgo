0cf697e4e97f790849f50e11599bf8deacc33df6
AdarshGrewal <adarshgrewal@gmail.com>
2024-09-13 15:27:24 +0600

    ginkgo: Build missing DRM library
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.3-service.widevine": cannot locate symbol "CBS_init" referenced by "/vendor/lib64/libwvhidl.so"...

--- device.mk
+++ device.mk
@@ -139,6 +139,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # DRM
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.4.vendor
 
